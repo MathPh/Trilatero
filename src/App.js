aa263077d1fbdff564f0f9407fae7b72c917748e
MathPh <matheusph27@hotmail.com>
2021-09-12 01:27:19 -0300

    Mudanca do tabuleiro para o Konva e aplicada modularização nas posicoes do tabuleiro

--- src/App.js
+++ src/App.js
@@ -2,76 +2,8 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import Jogador from './componentes/Jogador.js';
 import Carta from './componentes/Carta';
+import Tabuleiro from './componentes/Tabuleiro.js';
 
-const Canvas = props => {
-  
-  const canvasRef = React.useRef(null)
-  var context = null
-  useEffect(() => {
-    const canvas = canvasRef.current
-    if(context === null){
-      context = canvas.getContext('2d')
-    }
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
-
-    var tabuleiro = [[]]
-
-    for (let index = 0; index < props.tam; index++) {
-
-      for (let index2 = 0; index2 < 2*index+1; index2++) {
-        if(index2%2==0)
-          desenhaTri(index2, index,props.tam)
-      }
-    }
-    
-    
-  })
-
-  function desenhaQuad(x,y) {
-    context.moveTo(props.offset + props.size*(x), props.offset+props.size*(y))
-    context.lineTo(props.offset + props.size*(x+1), props.offset+props.size*(y))
-    context.stroke()
-    context.lineTo(props.offset + props.size*(x+1), props.offset+props.size*(y+1))
-    context.stroke()
-    context.lineTo(props.offset + props.size*(x), props.offset+props.size*(y+1))
-    context.stroke()
-    context.lineTo(props.offset + props.size*(x), props.offset+props.size*(y))
-    context.stroke()
-  }
-  function desenhaTri(x,y, tam) {
-    /*(lado * nTri) / 2
-    (lado * nTri) / 2 - lado/2 == (lado * (nTri-1))/2
-    (lado * nTri) / 2 - 2*lado/2
-    (lado * nTri) / 2 - 3*lado/2*/
-
-    const h = (props.size*Math.sqrt(3))/2
-
-    /*  */
-    context.moveTo(props.offset+ (props.size*(tam-y))/2 + (props.size*(x/2)),props.offset +h*(y))
-    context.lineTo(props.offset+ (props.size*(tam-y))/2 + (props.size*(x/2-0.5)), props.offset +h*(y+1))
-    context.stroke()
-    context.lineTo(props.offset+ (props.size*(tam-y))/2 + (props.size*(x/2+0.5)), props.offset +h*(y+1))
-    context.stroke()
-    context.lineTo(props.offset+ (props.size*(tam-y))/2 + (props.size*(x/2)),props.offset +h*(y))
-    context.stroke()
-
-    /*/  
-
-    context.moveTo(props.offset+ (props.size*(tam-y))/2,props.offset +h*(y))
-    context.lineTo(props.offset+ (props.size*(tam-y))/2 - props.size/2, props.offset +h*(y+1))
-    context.stroke()
-    context.lineTo(props.offset+ (props.size*(tam-y))/2 + props.size/2, props.offset +h*(y+1))
-    context.stroke()
-    context.lineTo(props.offset+ (props.size*(tam-y))/2,props.offset +h*(y))
-    context.stroke()
-    /*  */
-
-  }
-  
-  return <canvas ref={canvasRef} width={props.size*props.tam} height={((props.size*Math.sqrt(3))/2)*props.tam} style={{border:"1px solid #000000"}, {background: "#FFFFFF"}}/>
-}
 
 
 
@@ -96,7 +28,7 @@ function App() {
           <button onClick={() => {handleTamAum()}}>+</button>
           <button onClick={() => {handleTamDim()}}>-</button>
         </div>
-      <Canvas tam={tam} offset={0} size={50} />
+      <Tabuleiro tam={tam} offset={0} size={50} />
       <Jogador/>
       <Carta/>
       </header>
---
+++ src/componentes/Tabuleiro.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import { Stage, Layer, RegularPolygon, Text } from 'react-konva';
+
+class PosicaoCls {
+  constructor(id, x,y,paridade,canvas,lado){
+    this.id = id;
+    this.x = x
+    this.y = y
+    this.paridade = paridade % 2
+    this.canvas = canvas
+    this.lado = lado //global?
+
+    //return <RegularPolygon />
+    //cor?
+
+
+  }
+
+  draw3(){
+    var h = (this.lado*Math.sqrt(3))/2;
+    if(this.paridade % 2 == 1){
+
+      this.canvas.lineWidth = 1;
+      this.canvas.beginPath();
+
+      this.canvas.moveTo (this.x-this.lado/2,this.y-h);
+      this.canvas.lineTo (this.x+this.lado/2,this.y-h);
+      this.canvas.lineTo (this.x,this.y);
+      this.canvas.closePath ();
+      this.canvas.stroke ();
+
+
+    }else {
+      this.canvas.lineWidth = 1;
+      this.canvas.beginPath();
+
+      this.canvas.moveTo (this.x,this.y-h);
+      this.canvas.lineTo (this.x-this.lado/2,this.y);
+      this.canvas.lineTo (this.x+this.lado/2,this.y);
+      this.canvas.closePath ();
+      this.canvas.fillStyle   ="red";
+      this.canvas.fill ();
+    }
+
+}
+
+  draw(){
+
+    var h = (this.lado*Math.sqrt(3))/2;
+    this.obj.lineWidth = 1;
+
+    if(this.paridade % 2 == 1){
+
+      this.obj.moveTo (this.x-this.lado/2,this.y-h);
+      this.obj.lineTo (this.x+this.lado/2,this.y-h);
+      this.obj.lineTo (this.x,this.y);
+
+
+    }else {
+
+      this.obj.moveTo (this.x,this.y-h);
+      this.obj.lineTo (this.x-this.lado/2,this.y);
+      this.obj.lineTo (this.x+this.lado/2,this.y);
+
+    }
+
+    this.obj.closePath ();
+    this.obj.className = "abc";
+    this.canvas.stroke (this.obj);
+
+  }
+
+} 
+
+const Tabuleiro = props => {
+  
+    const canvasRef = React.useRef(null)
+
+    const [tabuleiro, setTabuleiro] = React.useState([]);
+
+    var context = null
+    useEffect(() => {
+      
+  
+      
+      var ii = 0
+      setTabuleiro([])
+      for (let index = 0; index < props.tam; index++) {
+        for (let index2 = 0; index2 < 2*index+1; index2++) {
+          //tabAux.push(new PosicaoCls(index.toString() + "-" + index2.toString(), props.tam/2*props.size + ((index2 - index)*props.size/2), index*(props.size*Math.sqrt(3))/2, index2, context, props.size))
+          
+          setTabuleiro(prev => ([...prev, new PosicaoCls(index.toString() + "-" + index2.toString(), props.tam/2*(props.size*(Math.sqrt(3))) + ((index2 - index)*props.size*(Math.sqrt(3))/2), props.size*1.5*index-(index2%2==0?0:props.size/2), index2, context, props.size)]))
+          ii++
+        }
+      }
+      
+      
+      console.log("Saida")
+      
+    },[props.tam])
+  
+    
+    return <Stage width={props.size*props.tam} height={((props.size*Math.sqrt(3))/2)*props.tam} >
+      <Layer>
+      {tabuleiro.map((tab) => (
+          <RegularPolygon
+            key={tab.id}
+            id={tab.id}
+            x={tab.x}
+            y={tab.y}
+            rotation={tab.paridade == 0?0:180}
+            sides={3}
+            radius={tab.lado}
+            fill={tab.paridade == 0?"#89b717":"#900000"}
+          />
+        ))}
+      </Layer>
+    </Stage>
+
+    //return <canvas ref={canvasRef} width={props.size*props.tam} height={((props.size*Math.sqrt(3))/2)*props.tam} style={{border:"1px solid #000000"}, {background: "#FFFFFF"}}/>
+  }
+  
+  export default Tabuleiro;
+  
