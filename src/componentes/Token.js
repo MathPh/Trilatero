749b53f8b62c51dc1b585bb7fe01ea077a9cf6d3
Falnésio Ghander Soares Borges <36005374+Falnesio@users.noreply.github.com>
2021-09-13 01:06:19 +0000

    aplicar imagem de token

--- src/componentes/Token.js
+++ src/componentes/Token.js
@@ -1,14 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { RegularPolygon} from 'react-konva';
+import useImage from 'use-image';
+import imagem from '../assets/token.jpg'
 
 const Token = props => {
+    const [image] = useImage(imagem)
     const [posicao, setPosicao] = React.useState(props.posicaoIni);
     useEffect(() => {
         console.log(props.posicaoIni)
         setPosicao(props.posicaoIni)
     },[props.posicaoIni])
 
-    return <RegularPolygon sides={6} x={posicao.posicao.x} y={posicao.posicao.y} radius={10} fill={"#000"} />
+    return <RegularPolygon 
+    sides={6} 
+    x={posicao.posicao.x} 
+    y={posicao.posicao.y} 
+    radius={20} 
+    fillPatternImage = {image}
+    fillPatternOffsetX = {-250}
+    fillPatternOffsetY = {-300}
+    fillPatternScaleX = {0.1}
+    fillPatternScaleY = {0.1}
+    draggable = {1}
+    />
 }
 
 export default Token;
